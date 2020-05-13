27f2a0e0894b5d75f5d5cfde9ada7e345a3ac038
Chantal S <chantal.shaib@gmail.com>
2020-05-13 16:19:18 -0400

    add support for augmented embeddings with glove and optional classifier ontop of bert forward method

--- _model/BertForAbstractScreening.py
+++ _model/BertForAbstractScreening.py
@@ -1,7 +1,8 @@
-import torch 
+import torch
+import numpy as np
+import torchtext 
 from transformers import *
 from torch.nn import CrossEntropyLoss
-import numpy as np
 
 class BertForAbstractScreening(nn.Module):
   
@@ -13,20 +14,25 @@ class BertForAbstractScreening(nn.Module):
             num_labels {int} -- number of labels for the data (default: {2})
         """          
         super(BertForSequenceClassification, self).__init__()
+    
         self.num_labels = num_labels
         # output: last_hidden_state, pooler_output, hidden_states
         self.bert = BertModel.from_pretrained(pretrained_weights, output_hidden_states=True)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        # can change classifier here 
-        self.classifier = nn.Linear(config.hidden_size, num_labels)
+        self.glove = torchtext.vocab.GloVe(name="6B", dim=50)  
+
+        ########### NOTE: add or change classifier on top of BERT here ###########
+        # self.classifier = nn.Linear(config.hidden_size, num_labels)
+
         nn.init.xavier_normal_(self.classifier.weight)
 
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, augmented=None):
+    def forward(self, input_ids, augment_ids, token_type_ids=None, attention_mask=None, labels=None, augmented=None):
         """ Forward method of the BERT model. 
 
         Arguments:
             input_ids {torch.tensor} -- unique identifier of the input.
+            augment_ids {torch.tensor} -- unique identifier of the augmented input.
 
         Keyword Arguments:
             token_type_ids {torch.tensor} -- sentence type. (default: {None})
@@ -39,7 +45,20 @@ class BertForAbstractScreening(nn.Module):
         """        
         _, pooled_output, hidden_states = self.bert(input_ids, token_type_ids, attention_mask)
         pooled_output = self.dropout(pooled_output)
-        # send in the augmented embeddings here
-        logits = self.classifier(pooled_output)
 
-        return logits
+        augmented_emb = []
+
+        for element in augment_list: 
+            augmented_emb.append(self.glove(element))
+        
+        # get an average of all the items in the augmented embeddings
+        avg_emb = torch.mean(torch.stack(augmented_emb), dim=0)
+
+        # concat the augementation to the pooled output  
+        pooled_output_augmented = torch.cat((pooled_output, avg_emb), dim=0)
+
+        ########### NOTE, OPTIONAL: send in the augmented embeddings through classifier ###########
+        # logits = self.classifier(pooled_output_augmented)
+        # return logits
+
+        return pooled_output_augmented
